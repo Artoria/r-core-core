37c3f2ff82cf230d4827d0b64c53d164757dfbc0
Artoria <pochy2008@qq.com>
2014-04-30 18:01:22 +0800

    Update RTCEnv.rb

--- RTCEnv.rb
+++ RTCEnv.rb
@@ -1,63 +1,24 @@
-require 'erb'
-class RTCEnv
-  IOStack = Struct.new(:next, :io)
-  attr_accessor :io
-  def initialize(&block)
-    self.io = {}
-    @argss = {}
-    self.begin
-    instance_exec &block
+env = RTCEnv.new do 
+  def forrange(a, b)
+    id = random
+    pushargs id
+    write "for(int #{id}=#{a}; #{id}<#{b}; ++#{id}){\n"
+    caption = where
+    write "}"
+
+    if block_given?
+      yield id
+      popargs
+    else
+      popargs
+      caption
+    end
   end
 
-  def begin
-    io[:$top] = ""
-    @stack = IOStack.new(nil, io[:$top])   
-  end
-
-  def random
-    @id ||= 0
-    @id += 1
-    "__id#{@id}"
-  end
-
-  def where(caption = random)
-    @argss[caption] = @args[-1]
-    write "<%= render #{caption.inspect} %>"
-  end
-
-  def end
-    render :$top
-  end
-
-  def render caption
-    ERB.new(io[caption]).result(binding)
-  end
-
-  
-  def write *a
-    @stack.io << a.join
-  end
-
-  def pushargs(*a)
-    @args ||= []
-    @args << a
-    a  
-  end
-
-  def popargs
-    @args.pop  
-  end
-
-  def file(a, b)
-    write "FILE *fp = fopen(#{a.inspect}, #{b.inspect});\n"
-    yield *pushargs("fp")
-    popargs
-    write "fclose(fp);\n"
-  end
-
-  def reopen(caption)
-    @stack = IOStack.new(@stack, io[caption] ||= "")
-    yield *@argss[caption]
-    @stack = @stack.next
-  end
+ 
+  forrange(1, 5) do |id| 
+    write "printf(\"%d\", #{id});\n"
+  end 
 end
+
+puts env.end
