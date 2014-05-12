8059923ff1dc39577be808316df22464e7abf419
Artoria <pochy2008@qq.com>
2014-05-12 14:14:48 +0800

    Update rcore.rb

--- rcore.rb
+++ rcore.rb
@@ -1,12 +1,7 @@
-require 'erb'
-class RCore
-  attr_accessor :io
-  def initialize
-    self.io = {}
-  end
 
-  def render(a, b)
-    io[:<<] << ERB.new(a).result(b.instance_eval{binding})
+class RCore
+  def render(a, b = self)
+    ERB.new(resolve(a)).result(b.instance_eval{binding})
   end
 
   def resolve(a)
@@ -14,5 +9,14 @@ class RCore
   end
 end
 
-
-
+class RQ < RCore
+  def initialize(dir)
+    @dir = File.join("./R", dir)
+  end
+  def resolve(f)
+    File.read(File.join(@dir, f) + ".txt").force_encoding("UTF-8")
+  end
+  def value
+    render 'main', self
+  end
+end
